44d92b341f4ce614a285d3bb18d7af3087e8b125
khushnood-ali-khan <khushnoodali287@gmail.com>
2025-01-27 19:09:57 +0500

    add to git

--- 7th_find_all.py
+++ 7th_find_all.py
@@ -1,2 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
+req = requests.get(url)
+
+soup = BeautifulSoup(req.text,"lxml")
+name = soup.find_all("a", {"class" : "title"})
+price = soup.find_all("h4", class_ = "price float-end card-title pull-right")
+desc = soup.find_all("p", class_ = "description card-text")
+
+l = len(name)
+# print(len(price))
+for i in range (l):
+    print(i,": ",name[i].string,": ",price[i].string,": ",desc[i].string)
