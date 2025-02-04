2c3f76135962688c0be0afba1c45e977e18f5518
khushnood-ali-khan <khushnoodali287@gmail.com>
2025-02-04 23:13:16 +0500

    Add to git

--- Practice/Wikipedia.py
+++ Practice/Wikipedia.py
@@ -7,10 +7,14 @@ req = requests.get(url)
 
 soup = BeautifulSoup(req.text,"lxml")
 paragraphs = soup.find_all("p")
+heading = soup.find_all("h2")
+
 
 paragraph_list = []
+heading_list = []
 
-for text in paragraphs :
+for text, title in zip (paragraphs, heading):
     paragraph_list.append(text.text)
+    heading_list.append(title.text)
 
-print(paragraph_list)
+print(paragraph_list,heading_list)
