ac828b5ddf923876d5f73bf491c4b7a5b30fd094
khushnood-ali-khan <khushnoodali287@gmail.com>
2025-02-05 16:14:11 +0500

    Add to git

--- Practice/Wikipedia.py
+++ Practice/Wikipedia.py
@@ -1,20 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 url = "https://en.wikipedia.org/wiki/Web_scraping"
 req = requests.get(url)
 
 soup = BeautifulSoup(req.text,"lxml")
-paragraphs = soup.find_all("p")
-heading = soup.find_all("h2")
-
-
-paragraph_list = []
-heading_list = []
-
-for text, title in zip (paragraphs, heading):
-    paragraph_list.append(text.text)
-    heading_list.append(title.text)
-
-print(paragraph_list,heading_list)
+body = soup.find("div", class_ = "mw-content-ltr mw-parser-output")
+print(body.text)
